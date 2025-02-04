fc298562740a3f9f4607cfb906fd17d91c7d5eff
Gil Andrey <gil_andrey@hotmail.com>
2025-02-04 11:46:04 -0300

    [Acesso a dados] - test introdution

--- libraryapi/src/main/java/io/github/springboot/libraryapi/Application.java
+++ libraryapi/src/main/java/io/github/springboot/libraryapi/Application.java
@@ -1,29 +1,12 @@
 package io.github.springboot.libraryapi;
 
-import io.github.springboot.libraryapi.model.Autor;
-import io.github.springboot.libraryapi.repository.AutorRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.time.LocalDate;
 
 @SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) {
-		var context = SpringApplication.run(Application.class, args);
-		AutorRepository repository = context.getBean(AutorRepository.class);
-
-		exemploSalvarRegistro(repository);
-	}
-
-	public static void exemploSalvarRegistro(AutorRepository autorRepository){
-		Autor autor = new Autor();
-		autor.setNome("Jose");
-		autor.setNacionalidade("Brasileiro");
-		autor.setDataNascimento(LocalDate.of(1950, 12, 15));
-
-		var autorSalvo = autorRepository.save(autor);
-		System.out.println("Autor salvo: " + autorSalvo);
+		SpringApplication.run(Application.class, args);
 	}
 }
---
+++ libraryapi/src/test/java/io/github/springboot/libraryapi/repository/AutorRepositoryTest.java
@@ -0,0 +1,26 @@
+package io.github.springboot.libraryapi.repository;
+
+import io.github.springboot.libraryapi.model.Autor;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.LocalDate;
+
+@SpringBootTest
+public class AutorRepositoryTest {
+
+    @Autowired
+    AutorRepository repository;
+
+    @Test
+    public void salvarTest() {
+            Autor autor = new Autor();
+            autor.setNome("Maria");
+            autor.setNacionalidade("Brasileiro");
+            autor.setDataNascimento(LocalDate.of(1980, 1, 10));
+
+            var autorSalvo = repository.save(autor);
+            System.out.println("Autor salvo: " + autorSalvo);
+        }
+}
