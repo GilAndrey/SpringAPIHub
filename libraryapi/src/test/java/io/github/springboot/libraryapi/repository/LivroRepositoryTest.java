daac529225ba0736dd9c9e9fa88bd4a4fe415ef8
Gil Andrey <gil_andrey@hotmail.com>
2025-02-05 13:30:36 -0300

    [Acesso a dados] - mappings test

--- libraryapi/src/test/java/io/github/springboot/libraryapi/repository/LivroRepositoryTest.java
+++ libraryapi/src/test/java/io/github/springboot/libraryapi/repository/LivroRepositoryTest.java
@@ -36,6 +36,29 @@ class LivroRepositoryTest {
         repository.save(livro);
     }
 
+    // Manual version - recommended
+    @Test
+    void salvarAutorELivroTest() {
+        Livro livro = new Livro();
+        livro.setIsbn("90029-84732");
+        livro.setPreco(BigDecimal.valueOf(100));
+        livro.setGenero(GeneroLivro.FANTASIA);
+        livro.setTitulo("Xo xing xho li");
+        livro.setDataPublicacao(LocalDate.of(1980, 2, 2));
+
+        Autor autor = new Autor();
+        autor.setNome("Xing lang in");
+        autor.setNacionalidade("Chinese");
+        autor.setDataNascimento(LocalDate.of(1994, 9, 11));
+
+        autorRepository.save(autor);
+
+        livro.setAutor(autor);
+
+        repository.save(livro);
+    }
+
+    // Test with cascade, not recommended
     @Test
     void salvarCascadeTest() {
         Livro livro = new Livro();
@@ -53,5 +76,32 @@ class LivroRepositoryTest {
         livro.setAutor(autor);
 
         repository.save(livro);
+    }
+
+    @Test
+    void atualizarAutorDoLivro() {
+
+        UUID idLivro = UUID.fromString("e7b25fdb-567c-410e-971f-159ac66fcb6c");
+        var livroParaAtualizar = repository.findById(idLivro).orElse(null);
+
+        UUID idAutor = (UUID.fromString("fe73bfd1-1b42-47f2-94ef-a90cddf31caa"));
+        Autor autor = autorRepository.findById(idAutor).orElse(null);
+
+        livroParaAtualizar.setAutor(autor);
+
+        repository.save(livroParaAtualizar);
+    }
+
+    @Test
+    void deletar() {
+        UUID idLivro = UUID.fromString("5ea1b9f4-8c05-4ed9-bed5-cf6361eb9425");
+        repository.deleteById(idLivro);
+    }
+
+    // Delete with cascade not recommend
+    @Test
+    void deletarCascade() {
+        UUID idLivro = UUID.fromString("1201a3c3-3d22-43bb-b6d9-aa6cf30d5e5c");
+        repository.deleteById(idLivro);
     }
 }
