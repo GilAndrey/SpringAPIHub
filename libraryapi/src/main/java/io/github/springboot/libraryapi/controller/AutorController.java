eabb21a2f1ee8353538393a5dc29c0ab73f72579
Gil Andrey <gil_andrey@hotmail.com>
2025-02-25 14:33:21 -0300

    [Api's resftul] - Getting autor details

--- libraryapi/src/main/java/io/github/springboot/libraryapi/controller/AutorController.java
+++ libraryapi/src/main/java/io/github/springboot/libraryapi/controller/AutorController.java
@@ -6,13 +6,12 @@ import io.github.springboot.libraryapi.service.AutorService;
 import jakarta.servlet.Servlet;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
+import java.util.Optional;
+import java.util.UUID;
 
 // http://localhost:8080/autores
 @RestController
@@ -37,5 +36,21 @@ public class AutorController {
                 .buildAndExpand(autorEntidade.getId())
                 .toUri();
         return ResponseEntity.created(location).build();
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<AutorDTO> obterDetalhes(@PathVariable("id") String id) {
+        var idAutor = UUID.fromString(id);
+        Optional<Autor> autorOptional = service.obterPorId(idAutor);
+        if (autorOptional.isPresent()) {
+            Autor autor = autorOptional.get();
+            AutorDTO dto = new AutorDTO(
+                    autor.getId(),
+                    autor.getNome(),
+                    autor.getDataNascimento(),
+                    autor.getNacionalidade());
+            return ResponseEntity.ok(dto);
+        }
+        return ResponseEntity.notFound().build();
     }
 }
