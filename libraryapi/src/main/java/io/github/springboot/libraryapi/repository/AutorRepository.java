558b31258bad25134c29fa28d6359291e26a37ba
Gil Andrey <gil_andrey@hotmail.com>
2025-02-25 15:09:43 -0300

    [Api's restful] - Autors search with filter

--- libraryapi/src/main/java/io/github/springboot/libraryapi/repository/AutorRepository.java
+++ libraryapi/src/main/java/io/github/springboot/libraryapi/repository/AutorRepository.java
@@ -3,7 +3,12 @@ package io.github.springboot.libraryapi.repository;
 import io.github.springboot.libraryapi.model.Autor;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface AutorRepository extends JpaRepository<Autor, UUID> {
+
+    List<Autor> findByNome(String nome);
+    List<Autor> findByNacionalidade(String nacionaldade);
+    List<Autor> findByNomeAndNacionalidade(String nome, String nacionaldade);
 }
