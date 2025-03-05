083025ba0f1c50492f9c25ab5bb69ad88bfb14b6
Gil Andrey <gil_andrey@hotmail.com>
2025-03-05 13:49:57 -0300

    [Bean validation] - Adding fields validation with bean

--- libraryapi/src/main/java/io/github/springboot/libraryapi/controller/dto/AutorDTO.java
+++ libraryapi/src/main/java/io/github/springboot/libraryapi/controller/dto/AutorDTO.java
@@ -1,14 +1,20 @@
 package io.github.springboot.libraryapi.controller.dto;
 
 import io.github.springboot.libraryapi.model.Autor;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 import java.time.LocalDate;
 import java.util.UUID;
 
 public record AutorDTO(
+
         UUID id,
+        @NotBlank(message = "campo obrigatorio")
         String nome,
+        @NotNull
         LocalDate dataNascimento,
+        @NotBlank
         String nacionalidade) {
 
 
