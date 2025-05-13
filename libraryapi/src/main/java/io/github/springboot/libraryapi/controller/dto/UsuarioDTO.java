dbb243c0e9177611a8c5a9c0d0990bf628a451e6
Gil Andrey <gil_andrey@hotmail.com>
2025-05-13 10:44:42 -0300

    [Security avancado] - Adicionando campo para salvar email no cadastro de usuario (Authentication Bug)

--- libraryapi/src/main/java/io/github/springboot/libraryapi/controller/dto/UsuarioDTO.java
+++ libraryapi/src/main/java/io/github/springboot/libraryapi/controller/dto/UsuarioDTO.java
@@ -1,6 +1,17 @@
 package io.github.springboot.libraryapi.controller.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
 import java.util.List;
 
-public record UsuarioDTO(String login, String senha, List<String> roles) {
+public record UsuarioDTO(
+        @NotBlank(message = "Campo obrigatorio")
+        String login,
+        @Email(message = "Email Invalid!")
+        @NotBlank(message = "Campo obrigatorio")
+        String email,
+        @NotBlank(message = "Campo obrigatorio")
+        String senha,
+        List<String> roles) {
 }
