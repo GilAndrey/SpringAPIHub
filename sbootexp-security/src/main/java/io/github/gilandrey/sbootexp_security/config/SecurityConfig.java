50ff5eae4f46d9fa905bfe6ad0191506a5035730
Gil Andrey <gil_andrey@hotmail.com>
2025-09-03 13:25:26 -0300

    [Spring security] - Credenciais de acesso (UserDetailsService / UserDetails)

--- sbootexp-security/src/main/java/io/github/gilandrey/sbootexp_security/config/SecurityConfig.java
+++ sbootexp-security/src/main/java/io/github/gilandrey/sbootexp_security/config/SecurityConfig.java
@@ -5,6 +5,12 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -22,5 +28,29 @@ public class SecurityConfig {
                 .httpBasic(Customizer.withDefaults()) // Permite a autorização via HTTP basic que é o pop-up do navegador (alert javascript)
                 .formLogin(Customizer.withDefaults()) // Habilita a autorização via o Formulario de login.
                 .build();
+    }
+
+    // Código para guardar as informações de usuario
+    @Bean
+    public UserDetailsService userDetailsService() { // Interface fundamental para encontrar informaçoes de usuario
+        UserDetails commonUser = User.builder() // Faz a criação de usuarios.
+                .username("user")
+                .password(passwordEncoder().encode("123")) // Passando password Encoder para codificar a senha
+                .roles("USER")
+                .build();
+
+        UserDetails adminUser = User.builder()
+                .username("admin")
+                .password(passwordEncoder().encode("admin"))
+                .roles("USER", "ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(commonUser, adminUser);
+    }
+
+    // Comando para Criptografar senhas
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder(); // BCrypt é o formato que a senha sera criptografada (recomendado)
     }
 }
